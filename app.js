8ab9ad8a6619c7df122b3bb52be0ab96fc78e654
ninajavier <ninajavier13@gmail.com>
2023-04-05 22:52:53 -0400

    Pokemon tests passed

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
 const express = require("express");
 const app = express();
+const pokemon = require("./models/pokemon.json");
 
+//ROUTES
 app.get("/", (req, res) => {
     res.send("Welcome 99 Pokemon")
 })
 
+// Poke-Express
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
+})
+
+app.get(`/pokemon/search`, (req, res) => {
+    const reqName = req.query.name;
+  const foundPokemon = pokemon.find(
+    (poke) => poke.name.toLowerCase() === reqName.toLowerCase()
+  );
+  if (foundPokemon) {
+    res.send([foundPokemon]);
+  } else {
+    res.send([]);
+  }
+});
+
+app.get("/pokemon/:indexOfArray", (req, res) => {
+    const { indexOfArray } = req.params
+    
+    if (pokemon[indexOfArray]) {
+        res.send(pokemon[indexOfArray]);
+    } else {
+        res.send("Sorry, no pokemon found at " + indexOfArray);
+    }
+})
+
+
+
+
+
+// New Project Name Generator
 app.get("/:verb/:adjective/:noun", (req, res) => {
     const { verb, adjective, noun } = req.params
     res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
 })
 
+
+// 99 Little Bugs In the Code
 let numberOfBugs = 99;
 
 app.get("/bugs", (req, res) => { 
@@ -33,4 +69,6 @@ app.get("*", (req, res) => {
     res.status(404).send(`This is not the page you are looking for`);
   });
 
+
+// EXPORT
 module.exports = app;
