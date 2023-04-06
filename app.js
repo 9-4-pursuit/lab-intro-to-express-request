bfc7b9b7f89a1a28f7f443fc547b264f833ec71a
Diegocarrero <diegocar2413@gmail.com>
2023-04-05 21:27:39 -0400

    pass all bugs tests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Welcome 99 Pokemon')
 })
 
 app.get('/:verb/:adjective/:noun', (req, res) => {
@@ -10,8 +10,21 @@ app.get('/:verb/:adjective/:noun', (req, res) => {
     res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
 })
 
-app.get('*', (req, res) => {
-    res.status(404).send('This is not the page you are looking for')
+app.get('/bugs', (req, res) => {
+    res.send(`<h1>99 little bugs in the code <a href="/bugs/101" >pull one down, patch it around</a></h1>`)
 })
+
+app.get('/bugs/:numberOfBugs', (req, res) => {
+    const { numberOfBugs } = req.params
+    Number(numberOfBugs) < 200 ? 
+        res.send(`${numberOfBugs} little bugs in the code <a href=${Number(numberOfBugs) + 2} >Pull one down, patch it around`)
+    : res.send(`<a href="/bugs">Too many bugs!! Start over!</a>`)
+})
+
+
+
+// app.get('*', (req, res) => {
+//     res.status(404).send('This is not the page you are looking for')
+// })
 
 module.exports = app;
