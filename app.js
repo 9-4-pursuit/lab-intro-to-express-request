8ce170760e6e899d2cf15d1bee0b3e28ae790b69
MaxWattan <mcwattanac@gmail.com>
2023-04-10 11:22:18 -0400

    lab passed

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const  express = require("express");
 const app = express()
+const pokemon = require("./models/pokemon.json");
+
+
 
 app.get("/", (req, res)=>{
     res.send("Welcome 99 Pokemon")
@@ -21,17 +24,45 @@ app.get("/bugs", (req, res) => {
     );
 })
 
-app.get("/bugs/:numberOfBugs", (req, res) => {
-    const { numberOfBugs } = req.params;
-    const buggy = Number(numberOfBugs);
+// app.get("/bugs/:numberOfBugs", (req, res) => {
+//     const { numberOfBugs } = req.params;
+//     const buggy = Number(numberOfBugs);
     
-    res.send(
-    `<p> ${buggy} little bugs in the code </p>
-     <a href= "${buggy < 200 ? buggy + 2 : "./"}" >${buggy < 200 
-     ? "Pull one down, patch it around"
-     : "Too many bugs!! Start over!"}</a>`
-    );
-});
+//     res.send(
+//     `<p> ${buggy} little bugs in the code </p>
+//      <a href= "${buggy < 200 ? buggy + 2 : "./"}" >${buggy < 200 
+//      ? "Pull one down, patch it around"
+//      : "Too many bugs!! Start over!"}</a>`
+//     );
+// });
+//OR
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    const { numberOfBugs } = req.params
+    bugsNum = Number(numberOfBugs)
+    if(bugsNum < 200) {
+        res.send(`<h1>"${bugsNum} little bugs in the code"</h1><a href="${bugsNum + 2}">Pull one down, patch it around</a>`)
+    }else{
+        res.send(`<a href="/bugs">Too many bugs!! Start over!</a>`)
+    }
+})
+
+app.get("/pokemon", (req, res)=>{
+    
+    res.send(pokemon)
+})
+
+app.get("/pokemon/search", (req, res)=>{
+    const { name } = req.query;
+    const selectPokemon = pokemon.find((pokemon) => pokemon.name.toLowerCase() === name.toLowerCase())
+    res.send(selectPokemon ? [selectPokemon] : [])
+    
+})
+
+app.get("/pokemon/:index", (req, res)=>{
+    const {index} = req.params;
+    res.send(pokemon[Number(index)] || "Sorry, no pokemon found at " + index)
+})
+
 
 app.get(("*"), (req, res) =>{
     res.status(404).send("This is not the page you are looking for")
